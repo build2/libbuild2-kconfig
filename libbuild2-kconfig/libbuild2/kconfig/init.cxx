e2da4e3ddf11b3d044f491110f3dd60b3cb485bb
Boris Kolpackov <boris@codesynthesis.com>
2020-08-17 11:43:48 +0200

    Implement configure/disfigure meta-operation hookup
    - The kconfig module must now be loaded in bootstrap.build.
    
    - It can also be explicitly loaded in root.build. Failed that, it will
      be automatically loaded after loading root.build.

--- libbuild2-kconfig/libbuild2/kconfig/init.cxx
+++ libbuild2-kconfig/libbuild2/kconfig/init.cxx
@@ -5,6 +5,8 @@
 #include <libbuild2/filesystem.hxx>
 #include <libbuild2/diagnostics.hxx>
 
+#include <libbuild2/config/utility.hxx>
+
 #include <libbuild2/kconfig/lkc.h>
 
 using namespace std;
@@ -13,33 +15,143 @@ namespace build2
 {
   namespace kconfig
   {
+    static const path def_file ("Kconfig");
+    static const path val_file ("config.kconfig");
+
+    static bool
+    configure_post (action a, const scope& rs)
+    {
+      if (a.operation () == default_id)
+      {
+        // Move config.kconfig.new to config.kconfig (see init() for details).
+        //
+        path vf (rs.out_path () / rs.root_extra->build_dir / val_file);
+        path nf (vf + ".new");
+
+        mvfile (nf, vf, 1);
+
+        nf += ".old";
+        if (exists (nf))
+        {
+          vf += ".old";
+          mvfile (nf, vf, 1);
+        }
+
+        return true;
+      }
+
+      return false;
+    }
+
+    static bool
+    disfigure_pre (action a, const scope& rs)
+    {
+      if (a.operation () == default_id)
+      {
+        // Move config.kconfig to config.kconfig.old (see init() for details).
+        //
+        path vf (rs.out_path () / rs.root_extra->build_dir / val_file);
+
+        // Similar logic to configure in init() below: if we have .new.old,
+        // use that (unfinished configure), otherwise, use .kconfig.
+        //
+        path f (vf + ".new");
+        bool e (exists (f));
+        if (!e)
+        {
+          f.make_base ();
+          e = exists (f);
+        }
+
+        if (e)
+        {
+          mvfile (f, vf + ".old", 1);
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    void
+    boot (scope& rs, const location& l, module_boot_extra& extra)
+    {
+      tracer trace ("kconfig::init");
+      l5 ([&]{trace << "for " << rs;});
+
+      context& ctx (rs.ctx);
+
+      // Register configure/disfigure hooks.
+      //
+      {
+        bool c (      ctx.bootstrap_meta_operation ("configure"));
+        bool d (!c && ctx.bootstrap_meta_operation ("disfigure"));
+        if (c || d)
+        {
+          if (!(c
+                ? config::configure_post (rs, &configure_post)
+                : config::disfigure_pre  (rs, &disfigure_pre)))
+            fail (l) << "config module must be loaded before kconfig";
+        }
+      }
+
+      // Initialize after (or explicitly during) loading root.build.
+      //
+      extra.init = module_boot_init::after;
+    }
+
     bool
     init (scope& rs,
-          scope& bs,
+          scope&,
           const location& l,
-          bool,
+          bool first,
           bool,
           module_init_extra&)
     {
       tracer trace ("kconfig::init");
       l5 ([&]{trace << "for " << rs;});
 
-      // We only support root loading.
-      //
-      // @@ TODO: and only in root.build
-      //
-      if (rs != bs)
-        fail (l) << "kconfig module must be loaded in project root";
+      if (!first)
+      {
+        warn (l) << "multiple kconfig module initializations";
+        return true;
+      }
 
       context& ctx (rs.ctx);
 
-      path df (rs.src_path () / rs.root_extra->build_dir / "Kconfig");
-      path vf (rs.out_path () / rs.root_extra->build_dir / "config.kconfig");
+      path df (rs.src_path () / rs.root_extra->build_dir / def_file);
+      path vf (rs.out_path () / rs.root_extra->build_dir / val_file);
 
       if (!exists (df))
         fail (l) << df << " does not exist";
 
       // (Re)configure if we are configuring.
+      //
+      // We have a complication: we need to load the configuration here and
+      // now since anything after (remainder of root.build, buildfiles) can
+      // depend on its values. On the other hand, the overall configure meta-
+      // operation may fail (for various reasons, including because the
+      // configuration is incorrect as determined by what we load after) and
+      // in this case we don't want to have config.kconfig "saved", similar to
+      // config.build. Yet, on the third hand, we don't want to just discard
+      // the configuration since it may have required a lot of effort to
+      // create (and the failure may have nothing to do with it). So here we
+      // are going to save the configuration as config.kconfig.new and in the
+      // post-configure hook we will move it to config.kconfig. We will also
+      // check if there is config.kconfig.new laying around and use that as
+      // the starter configuration.
+      //
+      // Also a few notes on the configurator semantics: conf's --oldconfig
+      // and --oldaskconfig are quite similar in that they both load the
+      // existing configuration if present. The difference is that --oldconfig
+      // only asks for "new" values (for some definition of "new") while
+      // --oldaskconfig asks for all values using the existing configuration's
+      // values as the default answers.
+      //
+      // The other configurators (mconf, qconf) implement a hybrid of the two
+      // modes in that they also load the existing configuration if any and
+      // then present the configuration tree/menu with this configuration's
+      // values letting the user tweak what they deem necessary.
       //
       // @@ TODO: support customzing mode/method via config.kconfig?
       //
@@ -47,13 +159,33 @@ namespace build2
       //
       if (ctx.current_mif->id == configure_id)
       {
-        //@@ We should only save the config to final place in hook during
-        //   execute. Also, kconfig-conf moves old config to *.old.
+        vf += ".new";
 
-        // What should be the default configuration mode? If the config file
-        // doesn't exist, then Q&A (--oldaskconfig). Otherwise, update the
-        // existing (--oldconfig).
+        // If config.kconfig.new already exists, use that. Otherwise, if
+        // config.kconfig exists, rename it to config.kconfig.new.
         //
+        // Note that in the former case we may also have the .new.old file
+        // which we should keep since configurators only create it if there
+        // are changes.
+        //
+        // What should the default configuration mode be? If the configuration
+        // doesn't exist, then --oldaskconfig. If the configuration does
+        // exists, then update it with --oldconfig. What if we have .new?
+        // Initially we treated it as the existing case but if some buildfile
+        // flags it as invalid, then on reconfigure we don't get a chance to
+        // change anything (since from --oldconfig's point of view nothing has
+        // changed). So now we use --oldaskconfig for this case.
+        //
+        bool e (false);
+        if (!exists (vf))
+        {
+          path f (vf.base ());
+          if ((e = exists (f)))
+          {
+            mvfile (f, vf, 2);
+            rmfile (ctx, vf + ".old", 3); // For good measure.
+          }
+        }
 
         //@@ Before importing and running the tool, we could check ourselves
         //   whether existing config is good.
@@ -82,7 +214,7 @@ namespace build2
         cstrings args {pp.recall_string ()};
 
         args.push_back ("-s");
-        args.push_back (exists (vf) ? "--oldconfig" : "--oldaskconfig");
+        args.push_back (e ? "--oldconfig" : "--oldaskconfig");
         args.push_back (df.string ().c_str ());
         args.push_back (nullptr);
 
@@ -309,7 +441,7 @@ namespace build2
 
     static const module_functions mod_functions[] =
     {
-      {"kconfig", nullptr, init},
+      {"kconfig", &boot,  &init},
       {nullptr,   nullptr, nullptr}
     };
 
